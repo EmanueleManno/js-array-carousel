4a55a480b8b28fded08b099f35ab284414ea49cc
EmanueleManno <mannus1994@gmail.com>
2023-06-01 16:15:40 +0200

    Milestone 2

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 //VEDO SE FUNZIONA 
 console.log('JS OK');
 
-//RECUPERIAMO LE IMMAGINI DAL DOM:
-const images = document.querySelectorAll('#carousel img');
+//RECUPERIAMO LA GALLERY DAL DOM:
+const gallery = document.querySelector('.gallery');
 
 //RECUPERIAMO I PULSANTI DAL DOM:
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
+
+//PREPARO LE IMMAGINI:
+const sources = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp']
+
+//PER OGNI SOURCE, CREO UN TAG IMG:
+for (let i = 0; i < sources.length; i++) {
+    const imageElement = document.createElement('img');
+    imageElement.src = sources[i];
+    imageElement.alt = `'immagine' ${i + 1}`;
+    gallery.appendChild(imageElement);
+}
+
+//RECUPERIAMO TUTTE LE IMMAGINI:
+const images = document.querySelectorAll('#carousel img');
 
 //PREPARO LA GESTIONE DELL'INDICE:
 let currentIndex = 0;
