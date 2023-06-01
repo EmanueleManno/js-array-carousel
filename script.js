2360f199fed453dd2c4b65bdb4b90e43e639da5f
EmanueleManno <mannus1994@gmail.com>
2023-06-01 15:11:14 +0200

    Javascript Milestone 1

--- script.js
+++ script.js
@@ -1,2 +1,44 @@
 //VEDO SE FUNZIONA 
-console.log('JS OK')
+console.log('JS OK');
+
+//RECUPERIAMO LE IMMAGINI DAL DOM:
+const images = document.querySelectorAll('#carousel img');
+
+//RECUPERIAMO I PULSANTI DAL DOM:
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+//PREPARO LA GESTIONE DELL'INDICE:
+let currentIndex = 0;
+
+//IMPOSTIAMO LA PRIMA IMMAGINE COME QUELLA ATTIVA:
+images[currentIndex].classList.add('active');
+
+//METTO IN ASCOLTO IL NEXT:
+nextButton.addEventListener('click', function () {
+    if(currentIndex === images.length - 1) return;
+
+    //RIMUOVERE LA CLASSE ATTIVA DALL'IMMAGINE CORRISPONDENTE ALL'INDICE:
+    images[currentIndex].classList.remove('active');
+
+    //INCREMENTO L'INDICE:
+    currentIndex++;
+
+    //AGGIUNGO LA CLASSE ATTIVA ALL'IMMAGINE CORRISPONDENTE AL NUOVO INDICE:
+    images[currentIndex].classList.add('active');
+
+});
+
+//METTO IN ASCOLTO IL PREV:
+prevButton.addEventListener('click', function () {
+    if (!currentIndex) return;
+
+    //RIMUOVERE LA CLASSE ATTIVA DALL'IMMAGINE CORRISPONDENTE ALL'INDICE:
+    images[currentIndex].classList.remove('active');
+
+    //DECREMENTO L'INDICE:
+    currentIndex--;
+
+    //AGGIUNGO LA CLASSE ATTIVA ALL'IMMAGINE CORRISPONDENTE AL NUOVO INDICE:
+    images[currentIndex].classList.add('active');
+})
